2b4a263c172a1b559bce40757e1bd619a7ee8fdd
crawlin <crawlin@uw.edu>
2026-03-28 18:41:19 -0700

    added deployment workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  output: 'export',
+  basePath: '/EducationPortfolio',
+  images: {
+    unoptimized: true,
+  },
+}
+
+
 
 export default nextConfig;
